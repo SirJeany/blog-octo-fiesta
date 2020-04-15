2b64cfdb21ff224c6d4bef632b6101c6964e8ad4
SirJeany <jmvanriet1@gmail.com>
2020-04-15 15:52:07 +0200

    added skeletons for view and create

--- routes/create-post.js
+++ routes/create-post.js
@@ -1,5 +1,12 @@
 let express = require('express');
 let router = express.Router();
 
+router.get('/', function(req, res, next){
+    let data = {
+        title: "Create Post"
+    }
+
+    res.render('create-post', data);
+});
 
 module.exports = router;
--- routes/view-post.js
+++ routes/view-post.js
@@ -4,11 +4,12 @@ let allPosts = require('../myPosts.json');
 
 router.get('/:postId', function(req, res, next){
     id = req.params.postId;
-    post = allPosts[id-1];
+    post = allPosts.myPosts[id-1];
     let data = {
         title: post.title,
         content: post.content
     }
+    res.render('view-post', data);
 });
 
 module.exports = router;
