eb3364cfbd0a40a93c439d1e2660313eabe2db39
SirJeany <jmvanriet1@gmail.com>
2020-04-15 15:29:19 +0200

    adding mock first post

--- routes/view-post.js
+++ routes/view-post.js
@@ -1,5 +1,14 @@
 let express = require('express');
 let router = express.Router();
+let allPosts = require('../myPosts.json');
 
+router.get('/:postId', function(req, res, next){
+    id = req.params.postId;
+    post = allPosts[id-1];
+    let data = {
+        title: post.title,
+        content: post.content
+    }
+});
 
 module.exports = router;
