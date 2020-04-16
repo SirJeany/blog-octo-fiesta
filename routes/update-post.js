db1a399d1819b84fcf7274ab0379ec41514b6c6b
SirJeany <jmvanriet1@gmail.com>
2020-04-16 11:53:13 +0200

    backend get done

---
+++ routes/update-post.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const myPosts = require('../myPosts.json').myPosts;
+
+router.get('/:postId', function(req, res, next){
+    let post = myPosts[req.params.postId-1];
+    let data = {
+        title: post.title,
+        content: post.content
+    }
+});
+
+module.exports = router
