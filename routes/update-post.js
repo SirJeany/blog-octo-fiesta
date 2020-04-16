497c3ed0e6e2440a243f58069975b024867cfe7f
SirJeany <jmvanriet1@gmail.com>
2020-04-16 12:21:23 +0200

    added the request Update

--- routes/update-post.js
+++ routes/update-post.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const myPosts = require('../myPosts.json').myPosts;
+const request = require('request');
 
 router.get('/:postId', function(req, res, next){
     let post = myPosts[req.params.postId-1];
@@ -11,5 +12,18 @@ router.get('/:postId', function(req, res, next){
 
     res.render('update-post', data);
 });
+
+
+router.post('/:postId', function(req, res, next){
+    request({
+        url: "http://localhost:8000/myPosts" + req.params.postId,
+        method: "UPDATE",
+        form: {
+            title: req.body.postTitle,
+            content: req.body.editordata
+        }
+    })
+});
+
 
 module.exports = router
