48ad716248b33f231683fe9eaa6262603135eb2e
SirJeany <jmvanriet1@gmail.com>
2020-04-20 18:19:48 +0200

    Mongo tests

--- mongo.js
+++ mongo.js
@@ -5,7 +5,7 @@ async function main() {
     const uri = "mongodb+srv://blog_man:s12572@blog-cluster-8ad2j.mongodb.net/test?retryWrites=true&w=majority";
 
     // Instance of the Mongo client
-    const client = new MongoClient(uri);
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
     // Use MongoClient to connect to our clusture. client.connect returns a promise.
     await client.connect();
@@ -26,3 +26,10 @@ async function main() {
 }
 
 main().catch(console.error);
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
