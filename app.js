351aeaaf4d063549df4348b29c0fae9513fad164
SirJeany <jmvanriet1@gmail.com>
2020-04-20 10:53:32 +0200

    Register Get working

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 let viewPostRouter = require('./routes/view-post');
 let createPostRouter = require('./routes/create-post');
 let updatePostRouter = require('./routes/update-post');
+let registerRouter = require('./routes/register');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/', indexRouter);
 app.use('/view-post', viewPostRouter);
 app.use('/create-post', createPostRouter);
 app.use('/update-post', updatePostRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/register.js
@@ -0,0 +1,35 @@
+let express = require('express');
+let router = express.Router();
+
+router.get('/', function(req, res){
+    let login_as = checkCookies(req.cookies);
+    let data = {
+        title: "Register",
+        login: login_as
+    }
+
+    res.render('register', data);
+});
+
+router.post('/', function(req, res){
+    // Add user to the user db and redirect to home page.
+});
+
+// Need to make this function centralised somewhere. Doesnt work app.js or in separate js...
+function checkCookies(cookies) {
+    console.log("Cookies: ", cookies);
+    let loggedInAs = cookies.loggedInAs;
+    try {
+      if(loggedInAs.length > 0) {
+        return loggedInAs;
+      } else {
+        return -1;
+      }
+    } catch (error) {
+      console.log('No cookie, so no data for login');
+      return -1;
+    }
+  }
+  
+
+module.exports = router;
