1bcbb41055f24396011c85443b2d460c9031d078
SirJeany <jmvanriet1@gmail.com>
2020-04-23 18:56:04 +0200

    reindent

--- routes/update-post.js
+++ routes/update-post.js
@@ -4,15 +4,15 @@ const myPosts = require('../myPosts.json').myPosts;
 const request = require('request');
 
 router.get('/:postId', function(req, res, next){
-    let post = myPosts[req.params.postId-1];
-    let login_as = checkCookies(req.cookies);
-    let data = {
-        title: post.title,
-        content: post.content,
-        login: login_as
-    }
-
-    res.render('update-post', data);
+  let post = myPosts[req.params.postId-1];
+  let login_as = checkCookies(req.cookies);
+  let data = {
+    title: post.title,
+    content: post.content,
+    login: login_as
+  }
+  
+  res.render('update-post', data);
 });
 
 // router.get('/:postId', function(req, res, next){
@@ -22,29 +22,29 @@ router.get('/:postId', function(req, res, next){
 // });
 
 router.post('/:postId', function(req, res, next){
-    console.log("req.body.postTitle = " + req.body.postTitle);
-    request({
-        url: "http://localhost:8000/myPosts/" + req.params.postId,
-        method: "PATCH",
-        form: {
-            title: req.body.postTitle,
-            content: req.body.editordata
-        }
-    },function(error, response, body){
-        console.log("The new body:", body);
-    });
-    
-    res.redirect('/view-post/'+req.params.postId);
-    
-    // TRIED--> Workaround for directing to updated post (user needs to refresh currently).
-    // let post = myPosts[req.params.postId-1];
-    // let data = {
-    //     postId: post.id,
-    //     title: post.title,
-    //     content: post.content
-    // }
-    // 
-    // res.render.apply('/view-post/'+req.params.postId, {message: "Successful", data});
+  console.log("req.body.postTitle = " + req.body.postTitle);
+  request({
+    url: "http://localhost:8000/myPosts/" + req.params.postId,
+    method: "PATCH",
+    form: {
+      title: req.body.postTitle,
+      content: req.body.editordata
+    }
+  },function(error, response, body){
+    console.log("The new body:", body);
+  });
+  
+  res.redirect('/view-post/'+req.params.postId);
+  
+  // TRIED--> Workaround for directing to updated post (user needs to refresh currently).
+  // let post = myPosts[req.params.postId-1];
+  // let data = {
+  //     postId: post.id,
+  //     title: post.title,
+  //     content: post.content
+  // }
+  // 
+  // res.render.apply('/view-post/'+req.params.postId, {message: "Successful", data});
 });
 
 function checkCookies(cookies) {
--- routes/view-post.js
+++ routes/view-post.js
@@ -3,25 +3,25 @@ let router = express.Router();
 let allPosts = require('../myPosts.json');
 
 router.get('/:postId', function(req, res, next){
-    // The workaround for showing the updates on the page after redirecting.
-    // Otherwise after maling the patch request, the user would need to refresh page.
-    setTimeout(() => {
-        id = req.params.postId;
-        post = allPosts.myPosts[id-1]; //We should look though and find the id rather than this
-        let login_as = checkCookies(req.cookies);
-        let data = {
-            postId: id,
-            title: post.title,
-            content: post.content,
-            featered_img: post.featered_img,
-            author: post.author,
-            published_date: post.published_date,
-            login: login_as
-        }
-        res.render('view-post', data);
-
-    }, 200);
-    // Note - 150ms is the lowest Ive tested with that works. 100ms is too quick for instance.
+  let login_as = checkCookies(req.cookies);
+  // The workaround for showing the updates on the page after redirecting.
+  // Otherwise after maling the patch request, the user would need to refresh page.
+  setTimeout(() => {
+    id = req.params.postId;
+    post = allPosts.myPosts[id-1]; //We should look though and find the id rather than this
+    let data = {
+      postId: id,
+      title: post.title,
+      content: post.content,
+      featered_img: post.featered_img,
+      author: post.author,
+      published_date: post.published_date,
+      login: login_as
+    }
+    res.render('view-post', data);
+    
+  }, 200);
+  // Note - 150ms is the lowest Ive tested with that works. 100ms is too quick for instance.
 });
 
 function checkCookies(cookies) {
