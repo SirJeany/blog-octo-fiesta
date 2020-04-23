61a2164c9db8d87aa257d9d63f886b868b6de3b6
SirJeany <jmvanriet1@gmail.com>
2020-04-23 11:39:33 +0200

    added posts-carousel

---
+++ public/javascripts/post-carousel.js
@@ -0,0 +1,30 @@
+'use strict';
+
+$(document).ready(function() {
+    $(".post-carousel").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        centerPadding: '200px',
+        // prevArrow: false,
+        // nextArrow: '.next-course',
+        arrows: true,
+        dots: true,
+        pauseOnHover: true
+        // responsive: [
+        // {
+        //     breakpoint: 900,
+        //     settings: {
+        //     slidesToShow: 2
+        //     }
+        // },
+        // {
+        //     breakpoint: 560,
+        //     settings: {
+        //     slidesToShow: 1
+        //     }
+        // }
+        // ]
+    });
+});
