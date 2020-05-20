abc28b1eba18cc0143b2986fea941e060d9e1806
SirJeany <jmvanriet1@gmail.com>
2020-05-20 10:58:36 +0200

    fixed cookie making copy when logging out of other than home

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -10,6 +10,6 @@ let logoutBtn = document.getElementById('logoutBtn');
 
 if(logoutBtn) {
     logoutBtn.addEventListener('click', () =>{ 
-        document.cookie = "checkLogin=;path=/;";
+        document.cookie = "checkLogin=;path=/;"; // The path=/ is important, otherwise the logout will set a cookie to nothing only at that specific path -> like /views 
     });
 }
