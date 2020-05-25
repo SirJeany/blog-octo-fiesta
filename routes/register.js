dc447a43a32601e8da5a2d33690c5c1c8c52356a
SirJeany <jmvanriet1@gmail.com>
2020-05-25 16:30:19 +0200

    Updated register to only use request

--- routes/register.js
+++ routes/register.js
@@ -15,46 +15,67 @@ router.get('/', function(req, res){
   res.render('register', data);
 });
 
+// express-validator format for server-side validation:
 router.post('/', 
 [
+  // ToDo: stronger error checking with names?
+  check('firstName').isLength({min:2}),
+  check('lastName').isLength({min:2}),
   check('email').isEmail(), 
   check('password').isLength({min:3})
 ], 
-function(req, res){
+function(req, res, next){
+  // Check errors:
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     return res.status(422).json({ errors: errors.array() });
   }
 
-  // check if user exists:
-  if(checkExisitingUser(req.body.email)) {
-    console.log("User exsists");
-    res.render('register', {title: "register", message: "User with that email already exists", login: -1})
-  }
-  else {
-    let newUserID = allUsers.length + 1;
-    request({
-      url: "http://localhost:8000/allUsers/",
-      method: "POST",
-      form: {
-        id: newUserID,
-        first_name: req.body.firstName,
-        last_name: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password,
-        permissions: "RUM",
-        subscribed_to_newsletter: req.body.newsletterCheckbox ? true : false
+  let found = false;
+  let userCount = 0;
+  const email = req.body.email;
+  // First, make a get request to see if the user exists. We also want to know how many users there are currently
+  request({
+    uri: "http://localhost:8000/allUsers/",
+    method: "GET"
+  },
+  function(error, response, body){
+    const allUsers = JSON.parse(body); // get js object
+    // Length or size of an object = https://stackoverflow.com/questions/5223/length-of-a-javascript-object
+    // MDN: Object.keys() method returns an array of a given object's own enumerable property names, iterated in the same order that a normal loop would.
+    for (let i = 0; i < Object.keys(allUsers).length && !found; i++) {
+      const user = allUsers[i];
+      if(email === user.email) {
+        console.log("User found with email " + email + ": " + user.first_name)
+        found = true;
       }
-    }, function(error, response, body){
-      console.log("Error: ", error);
-      console.log("New user: ", JSON.stringify(body));
-      // Try to set the cookie details so that the user may be logged in immediately:
-      // This results in multiple headers being set :(
-      // res.cookie('checkLogin', JSON.stringify(body.first_name));
-    });
+    }
 
-    res.redirect('/');
-  }
+    // Set usercount for when we need to update the id...
+    userCount = Object.keys(allUsers).length;
+
+    // Now check if there was a user found:
+    if(found) {
+      res.render('register', {title: "register", message: "User with that email already exists", login: -1})
+    } else { // if no user with those credentials were found:
+      // Complete Post request:
+      request({
+        url: "http://localhost:8000/allUsers/",
+        method: "POST",
+        form: {
+          id: userCount + 1,
+          first_name: req.body.firstName,
+          last_name: req.body.lastName,
+          email: req.body.email,
+          password: req.body.password,
+          permissions: "RUM",
+          subscribed_to_newsletter: req.body.newsletterCheckbox ? true : false
+        }
+      }, function(error, response, body){
+        res.render('register', {title: "register", message: "Created new user", login: -1})
+      });
+    }
+  });
 });
 
 // Need to make this function centralised somewhere. Doesnt work app.js or in separate js...
@@ -74,15 +95,6 @@ function checkCookies(cookies) {
     console.log('No cookie, so no data for login');
     return -1;
   }
-}
-
-// Find out if the user exists already...
-function checkExisitingUser(email) {
-  let found = false;
-  for (let i = 0; i < allUsers.length && !found; i++) {
-    allUsers[i].email == email ? found = true : 0;
-  }
-  return found;
 }
   
 
