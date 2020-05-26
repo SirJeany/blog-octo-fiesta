ad47d5f46f613524b6a40425fa65de5931879249
SirJeany <jmvanriet1@gmail.com>
2020-05-26 13:14:56 +0200

    Visual changes - View post layout, Button descriptions and sizes

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const heroImageDiv = document.getElementById('heroImage');
-let imgH = heroImageDiv.offsetHeight;
-let imgW = heroImageDiv.offsetWidth;
-let heroImage = document.createElement('img');
+// For the unsplash hero image:
+// const heroImageDiv = document.getElementById('heroImage');
+// let imgH = heroImageDiv.offsetHeight;
+// let imgW = heroImageDiv.offsetWidth;
+// let heroImage = document.createElement('img');
 
-heroImage.setAttribute('src', `http://unsplash.it/${imgW}/${imgH}`);
-heroImage.classList.add('img-fluid');
-heroImageDiv.appendChild(heroImage);
+// heroImage.setAttribute('src', `http://unsplash.it/${imgW}/${imgH}`);
+// heroImage.classList.add('img-fluid');
+// heroImageDiv.appendChild(heroImage);
 
