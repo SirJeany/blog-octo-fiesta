5db61a0a3b80dfaba7d7d81ab678623c70d54d2e
SirJeany <jmvanriet1@gmail.com>
2020-05-31 11:28:14 +0200

    Adding skeleton for modal image

---
+++ public/javascripts/modal.js
@@ -0,0 +1,10 @@
+// https://www.w3schools.com/howto/howto_css_modal_images.asp
+'use strict';
+
+const myModal = document.getElementById('myModal');
+
+if(myModal) {
+    let projectImg = document.getElementById('projectImg');
+    let modalImg = document.getElementById('img01');
+
+}
