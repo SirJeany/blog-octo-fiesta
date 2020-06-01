b50f9dadda008007eb6c35c3fd6ebaf08b0408e1
SirJeany <jmvanriet1@gmail.com>
2020-06-01 13:55:38 +0200

    Modal complete

--- public/javascripts/modal.js
+++ public/javascripts/modal.js
@@ -6,5 +6,17 @@ const myModal = document.getElementById('myModal');
 if(myModal) {
     let projectImg = document.getElementById('projectImg');
     let modalImg = document.getElementById('img01');
+    let captionText = document.querySelector('.modal-image-caption');
 
+    projectImg.onclick = function() {
+        console.log(this)
+        myModal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+
+    // Close button
+    let spanClose = document.querySelector('.modal-close');
+    // Close the modal:
+    spanClose.onclick = function() { myModal.style.display = "none"; }
 }
