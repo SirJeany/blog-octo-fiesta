6fde0df17c5e0b78a793df8f0a678e17d27a74a9
SirJeany <jmvanriet1@gmail.com>
2020-06-06 18:22:27 +0200

    updated create post mobile view. Still to edit summernote error

--- public/javascripts/summernote.js
+++ public/javascripts/summernote.js
@@ -1,7 +1,22 @@
 // Using Summernote:
 $(document).ready(function() {
+    // $('#summernote').summernote({
+    
+    //     placeholder: 'Start your writing journey here...'
+    // });
+    
     $('#summernote').summernote({
-        
-        placeholder: 'Start your writing journey here...'
+        placeholder: 'Hello stand alone ui',
+        tabsize: 2,
+        height: 120,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture']],
+            ['view', ['fullscreen', 'codeview', 'help']]
+        ]
     });
 });
